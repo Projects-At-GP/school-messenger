963cf952234f4e03296d86ce1f51c5f4b230ebb1
Albert Unruh <keenan.noack@gmail.com>
2021-09-16 09:39:55 +0200

    first code

--- school-messenger/__init__.py
+++ school-messenger/__init__.py
@@ -1,5 +1,3 @@
 __author__ = "AlbertUnruh"
 __license__ = "MIT"
 __copyright__ = f"(c) {__author__}"
-
-from NAA import API
--- school-messenger/__main__.py
+++ school-messenger/__main__.py
@@ -1,1 +1,42 @@
+from NAA import API, APIRequest
 
+
+api = API("127.0.0.1", 666, name="School Messenger")
+
+
+@api.add
+def users(request: APIRequest):
+    return {"path": "users"}
+
+
+@users.add
+def info(request: APIRequest):
+    return {"path": "users/info"}
+
+
+@users.add
+def whoami(request: APIRequest):
+    return {"path": "users/whoami"}
+
+
+@users.add
+def registration(request: APIRequest):
+    return {"path": "users/registration"}
+
+
+@users.add
+def me(request: APIRequest):
+    return {"path": "users/me"}
+
+
+@me.add
+def token(request: APIRequest):
+    return {"path": "users/me/token"}
+
+
+@api.add
+def messages(request: APIRequest):
+    return {"path": "massages"}
+
+
+api(debug=True)
