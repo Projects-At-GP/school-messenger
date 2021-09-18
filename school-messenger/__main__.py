e1b2bb0bcdbb5c42f56bc2030c573b180db0a48a
Albert Unruh <keenan.noack@gmail.com>
2021-09-18 19:00:12 +0200

    First code with default return-values.

--- school-messenger/__main__.py
+++ school-messenger/__main__.py
@@ -1,42 +1,51 @@
-from NAA import API, APIRequest
+from NAA import APIRequest
+from NAA.web import API
 
 
-api = API("127.0.0.1", 666, name="School Messenger")
+api = API("127.0.0.1", 2345, name="School Messenger")
 
 
-@api.add
+@api.add(ignore_invalid_methods=True)
 def users(request: APIRequest):
-    return {"path": "users"}
+    return 403
 
 
-@users.add
+@users.add("GET")
 def info(request: APIRequest):
-    return {"path": "users/info"}
+    return {"name": "",
+            "id": ""}
 
 
-@users.add
+@users.add("GET")
 def whoami(request: APIRequest):
-    return {"path": "users/whoami"}
+    return {"name": "",
+            "id": ""}
 
 
-@users.add
+@users.add("POST", "DELETE")
 def registration(request: APIRequest):
-    return {"path": "users/registration"}
+    if request.method == "POST":
+        return 201, {"Token": ""}
+    if request.method == "DELETE":
+        return 204
 
 
-@users.add
+@users.add(ignore_invalid_methods=True)
 def me(request: APIRequest):
-    return {"path": "users/me"}
+    return 403
 
 
-@me.add
+@me.add("GET")
 def token(request: APIRequest):
-    return {"path": "users/me/token"}
+    return {"Token": ""}
 
 
-@api.add
+@api.add("POST", "GET")
 def messages(request: APIRequest):
-    return {"path": "massages"}
+    if request.method == "POST":
+        return 201, {"ID": ""}
+    if request.method == "GET":
+        return {"messages": [{"id": "", "content": "", "author": {"id": "", "name": ""}}]}
 
 
 api(debug=True)
