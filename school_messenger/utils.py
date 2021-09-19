0e27c256b9abcf44d70155955fbe9ed568cb351c
Albert Unruh <keenan.noack@gmail.com>
2021-09-19 17:15:44 +0200

    Add checker for User-Agent

--- school_messenger/utils.py
+++ school_messenger/utils.py
@@ -21,3 +21,23 @@ def is_authorized(request, *, authorization_key="Authorization"):
         return True
     except AssertionError:
         return False
+
+
+def has_user_agent(request, *, user_agent_key="User-Agent", min_user_agent_len=2):
+    """
+    Parameters
+    ----------
+    request: NAA.APIRequest
+    user_agent_key: str
+    min_user_agent_len: int
+
+    Returns
+    -------
+    bool
+    """
+    try:
+        assert (agent := request.headers.get(user_agent_key, "")), "Missing Header."
+        assert len(agent.split()) >= min_user_agent_len, "User Agent To Short."
+        return True
+    except AssertionError:
+        return False
