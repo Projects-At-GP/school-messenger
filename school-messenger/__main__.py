55e15d33cc11cdb01f43c1e5f9f82644704b8088
Albert Unruh <keenan.noack@gmail.com>
2021-09-19 17:00:11 +0200

    Add checks into code.

--- school-messenger/__main__.py
+++ school-messenger/__main__.py
@@ -1,23 +1,31 @@
 from NAA import APIRequest
 from NAA.web import API
 
+from .utils import is_authorized
+
 
 api = API("0.0.0.0", 3333, name="School Messenger")
 
 
 @api.add(ignore_invalid_methods=True)
-def users(request: APIRequest):
-    return 403
+def users(_):
+    ...
 
 
 @users.add("GET")
 def info(request: APIRequest):
+    if not is_authorized(request):
+        return 401
+
     return {"name": "",
             "id": ""}
 
 
 @users.add("GET")
 def whoami(request: APIRequest):
+    if not is_authorized(request):
+        return 401
+
     return {"name": "",
             "id": ""}
 
@@ -26,13 +34,17 @@ def whoami(request: APIRequest):
 def registration(request: APIRequest):
     if request.method == "POST":
         return 201, {"Token": ""}
+
     if request.method == "DELETE":
+        if not is_authorized(request):
+            return 401
+
         return 204
 
 
 @users.add(ignore_invalid_methods=True)
-def me(request: APIRequest):
-    return 403
+def me(_):
+    ...
 
 
 @me.add("GET")
@@ -42,8 +54,12 @@ def token(request: APIRequest):
 
 @api.add("POST", "GET")
 def messages(request: APIRequest):
+    if not is_authorized(request):
+        return 401
+
     if request.method == "POST":
         return 201, {"ID": ""}
+
     if request.method == "GET":
         return {"messages": [{"id": "", "content": "", "author": {"id": "", "name": ""}},
                              {"id": "", "content": "", "author": {"id": "", "name": ""}}]}
