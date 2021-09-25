a56a156f48e50cf1d909bb0a0f7a3cb69d6334a7
AlbertUnruh <keenan.noack@gmail.com>
2021-09-25 11:22:40 +0200

    Update `v0` to final and `v1` is now even to `v0`

--- school_messenger/versions/v0.py
+++ school_messenger/versions/v0.py
@@ -1,7 +1,7 @@
 from NAA import APIRequest
 from NAA.web import API
 
-from school_messenger.utils import has_user_agent, is_authorized, generate_id
+from school_messenger.utils import has_user_agent, is_authorized
 
 
 class V0:
@@ -14,27 +14,32 @@ class V0:
 
         @users.add("GET")
         def info(request: APIRequest):
-            return {"name": "",
-                    "id": str(generate_id(1))}
+            if not all([(query := request.get("query"))]):
+                return 400
+            return {"name": "", "id": ""}
 
         info.add_request_check(401)(is_authorized)
 
         @users.add("GET")
         def whoami(request: APIRequest):
-            return {"name": "",
-                    "id": str(generate_id(1))}
+            token = request.get("Authorization")  # noqa
+            return {"name": "", "id": ""}
 
         whoami.add_request_check(401)(is_authorized)
 
         @users.add("POST", "DELETE")
         def registration(request: APIRequest):
             if request.method == "POST":
+                if not all([(name := request.get("name", "")),
+                            (password := request.get("password", ""))]):
+                    return 400
                 return 201, {"Token": ""}
 
             if request.method == "DELETE":
                 if not is_authorized(request):
                     return 401
-
+                if not all([(password := request.get("password", ""))]):
+                    return 400
                 return 204
 
         @users.add(ignore_invalid_methods=True)
@@ -43,15 +48,23 @@ class V0:
 
         @me.add("GET")
         def token(request: APIRequest):
+            if not all([(name := request.get("name", "")),
+                        (password := request.get("password", ""))]):
+                return 400
             return {"Token": ""}
 
         @api.add("POST", "GET")
         def messages(request: APIRequest):
-            if request.method == "POST":
-                return 201, {"ID": str(generate_id(2))}
-
             if request.method == "GET":
-                return {"messages": [
-                    {"id": str(generate_id(2)), "content": "", "author": {"id": str(generate_id(1)), "name": ""}}]}
+                if not all([(content := request.get("content", ""))]):
+                    return 400
+                return {"messages": [{"id": "", "content": "", "author": {"id": "", "name": ""}}]}
+
+            if request.method == "POST":
+                if not all([(amount := request.get("amount", "20")).isnumeric(),
+                            (before := request.get("before", "-1")).isnumeric(),
+                            (after := request.get("after", "-1")).isnumeric()]):
+                    return 400
+                return 201, {"ID": ""}
 
         messages.add_request_check(401)(is_authorized)
--- school_messenger/versions/v1.py
+++ school_messenger/versions/v1.py
@@ -1,11 +1,70 @@
 from NAA import APIRequest
 from NAA.web import API
 
-from school_messenger.utils import has_user_agent, is_authorized, generate_id
+from school_messenger.utils import has_user_agent, is_authorized
 
 
 class V1:
     def __init__(self, api: API):
         api.add_global_request_check(401)(has_user_agent)
 
-        ...
+        @api.add(ignore_invalid_methods=True)
+        def users(_):
+            ...
+
+        @users.add("GET")
+        def info(request: APIRequest):
+            if not all([(query := request.get("query"))]):
+                return 400
+            return {"name": "", "id": ""}
+
+        info.add_request_check(401)(is_authorized)
+
+        @users.add("GET")
+        def whoami(request: APIRequest):
+            token = request.get("Authorization")  # noqa
+            return {"name": "", "id": ""}
+
+        whoami.add_request_check(401)(is_authorized)
+
+        @users.add("POST", "DELETE")
+        def registration(request: APIRequest):
+            if request.method == "POST":
+                if not all([(name := request.get("name", "")),
+                            (password := request.get("password", ""))]):
+                    return 400
+                return 201, {"Token": ""}
+
+            if request.method == "DELETE":
+                if not is_authorized(request):
+                    return 401
+                if not all([(password := request.get("password", ""))]):
+                    return 400
+                return 204
+
+        @users.add(ignore_invalid_methods=True)
+        def me(_):
+            ...
+
+        @me.add("GET")
+        def token(request: APIRequest):
+            if not all([(name := request.get("name", "")),
+                        (password := request.get("password", ""))]):
+                return 400
+            return {"Token": ""}
+
+        @api.add("POST", "GET")
+        def messages(request: APIRequest):
+            if request.method == "GET":
+                if not all([(content := request.get("content", ""))]):
+                    return 400
+                return {"messages": [{"id": "", "content": "", "author": {"id": "", "name": ""}}]}
+
+            if request.method == "POST":
+                if not all([(amount := request.get("amount", "20")).isnumeric(),
+                            (before := request.get("before", "-1")).isnumeric(),
+                            (after := request.get("after", "-1")).isnumeric()]):
+                    return 400
+                return 201, {"ID": ""}
+
+        messages.add_request_check(401)(is_authorized)
