8c44563ce316b9190b6d7941ca818150b85649d3
AlbertUnruh <keenan.noack@gmail.com>
2021-09-27 19:09:44 +0200

    `v2` is now ready!

--- school_messenger/versions/v2.py
+++ school_messenger/versions/v2.py
@@ -1,3 +1,19 @@
+from NAA import APIResponse
+from NAA.web import API
+
+
+__all__ = (
+    "V2",
+)
+
+
+# all from v1
 class V2:
-    def __init__(self, api):
-        pass
+    def __init__(self, api: API):
+        @api.add_global_response_check()
+        def lower_all_json(response: APIResponse):
+            json = {}
+            for k, v in response.response.items():
+                json[k.lower()] = v
+            response._response = json  # noqa
+            return response
